699a0a645d1b31a55b76cafc944e57187f9490b0
Odds <vitalik.zalozhnyj@gmail.com>
2023-11-21 17:45:48 +0500

    initialMain

---
+++ llm_data.py
@@ -0,0 +1,30 @@
+polar_bears = ['Polar bears are amazing animals that live in the Arctic.',
+                         "Polar bears have thick fur and a layer of fat to keep them warm in the cold climate.",
+                         'Polar bears hunt seals for food, and can swim very well.',
+                         'Polar bears are big, with adult males weighing up to 1,500 pounds!',
+                         'Polar bears have a long neck and powerful legs that help them move across the snow and ice.']
+
+overlord = ['Operation Overlord was the code name for the D-Day invasion of Normandy during World War II',
+                   "Operation Overlord was a massive military operation that involved thousands of ships, planes, and soldiers.",
+                   'The goal was to defeat Nazi Germany by landing on the beaches of Normandy and pushing the Germans back.',
+                   'The operation started on June 6, 1944, with an airborne assault by paratroopers and glider troops']
+
+arshavin = ['Andrey Arshavin is a former Russian professional footballer who played as an attacking midfielder or forward.',
+                   "Andrey Arshavin was born on May 29, 1981, in Leningrad, Soviet Union (now Saint Petersburg, Russia)",
+                   'Arshavin began playing football at a young age and joined the youth academy of Zenit Saint Petersburg in 1990.',
+                   'Arshavin quickly rose through the ranks and made his professional debut for the club in 1998, at the age of 17.']
+
+new_jersey = ['New Jersey is a state in the northeastern United States, known for its beautiful beaches, boardwalks, and amusement parks along the Atlantic coast.',
+                   "New Jersey is also home to many historic sites, including the Liberty Bell and Independence Hall in Philadelphia, which are just across the Delaware River from New Jersey.",
+                   'New Jersey is a diverse state with a mix of urban, suburban, and rural areas.',
+                   'New Jersey has a strong economy, with major industries such as finance, technology, and healthcare.',
+                   'Many famous companies, such as Johnson & Johnson and Prudential Financial, are based in New Jersey.']
+
+ubuntu = [
+        'Ubuntu is a popular open-source operating system that is based on the Linux distribution.',
+        ' Ubuntu is known for its simplicity, flexibility, and ease of use.',
+        'Free and open-source: Ubuntu is entirely free to download, use, and distribute, and its source code is openly available for anyone to review and modify.',
+        'User-friendly interface: Ubuntu has a user-friendly interface that makes it easy for both beginners and experienced users to navigate and perform various tasks.',
+        'Large community support: Ubuntu has a large and active community of users and developers who create and share tutorials, documentation, and support resources.',
+        'Ubuntu is a robust and versatile operating system that is well-suited for a wide range of users and use cases, from basic home PCs to advanced server deployments.'
+]
---
+++ no_google.py
@@ -0,0 +1,82 @@
+import nltk
+import json
+from nltk.tokenize import sent_tokenize, word_tokenize
+from sentence_transformers import SentenceTransformer, util
+
+
+
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+
+
+def get_relevant_refs(refs, source):
+
+
+    all_sentences = []
+    for j in refs:
+        sentences = sent_tokenize(j['snippet'])
+        all_sentences += sentences
+
+        embedding_source = model.encode(source, convert_to_tensor=True)
+        scores = []
+        for ref in all_sentences:
+            score = similarity(source, ref)
+            scores.append({
+                'ref': ref,
+                'score':score
+            })
+
+    candidates = sorted(list(filter(lambda r: r['score'] > 0.725, scores)),  key=lambda x:x['score'], reverse=True)
+    if len(candidates) > 1:
+        for i, c in enumerate(candidates):
+            current_refs = candidates[i+1:]
+            embedding_source = model.encode(c['ref'], convert_to_tensor=True)
+
+            for ref in current_refs:
+              score = similarity(c['ref'], ref['ref'])
+              if score > 0.45:
+                candidates.remove(ref)
+    elif len(candidates) == 0:
+            return None
+    return candidates
+
+def similarity(sentence_1, sentence_2):
+    embedding_source = model.encode(sentence_1, convert_to_tensor=True)
+    embedding_ref = model.encode(sentence_2, convert_to_tensor=True)
+    score = float(util.pytorch_cos_sim(embedding_source, embedding_ref)[0][0])
+    return score
+
+def run_no_google(google_outputs):
+    reliable_sentences = []
+    for sentence in google_outputs:
+        relevant_google_outputs = get_relevant_refs(sentence['google'], sentence['querry'])
+        if relevant_google_outputs == None:
+           new_sen1 = (sentence['querry'] + "  --> " +"Suspicious sentence. It is either meaningless or completely false")
+           reliable_sentences.append(new_sen1)
+           continue
+
+        # prompt = f'''Source: "{sentence['querry']}" '''
+        # for i, fact in enumerate(relevant_google_outputs):
+        #   prompt += f'Fact_{i+1}: "{fact}\n"'
+        #
+        # facts_name = "Fact_1"
+        # if len(relevant_google_outputs) > 1:
+        #     for i, fact in enumerate(relevant_google_outputs[1:]):
+        #         facts_name += f'and Fact_{i}'
+        # prompt += f'New_Source = Source modified to reflect {facts_name}. If the Source approximately reflects the {facts_name}, New_Source matches exactly Source. Do not type explanation, comments and score. Start type with: "New_Source: ...'
+        #
+        # llm_output = get_llm_output(prompt, 250)
+        # llm_output = llm_output.strip('\n. "').split('New_Source:')[-1].strip()
+
+
+
+        score = similarity(sentence['querry'], relevant_google_outputs)
+        if score < 0.60:
+          new_sen = sentence['querry'] + "   --> " +"Something went wrong!"
+          reliable_sentences.append(new_sen)
+        elif score < 0.90:
+          new_sen = sentence['querry'] + "   -->  " + str(score)
+          reliable_sentences.append(new_sen)
+        else:
+           new_sen = sentence['querry'] + "  --> " + "Absolutely correct!"
+           reliable_sentences.append(new_sen)
+    return(reliable_sentences)
